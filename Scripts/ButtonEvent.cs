dc6cf833abd02198e67eff0bc75664adcae05426
lizhen <MuziSoucen001@outlook.com>
2016-04-13 17:33:54 +0800

    add assets

---
+++ Scripts/ButtonEvent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonEvent : MonoBehaviour
+{
+	public void EnterTetris ()
+	{
+		SceneManager.LoadScene ("Tetris");
+	}
+
+	public void EnterGamesStart ()
+	{
+		SceneManager.LoadScene ("GamesStart");
+	}
+
+	public void EnterPlaneWar ()
+	{
+		SceneManager.LoadScene ("PlaneWar");
+	}
+
+	public void GameOver ()
+	{
+		Application.Quit ();
+	}
+}
